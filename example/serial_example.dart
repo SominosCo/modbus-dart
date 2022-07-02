258f258e3470645dc980c2abf1642e5ca4d2c741
Peter McGaughey <peter.mcgaughey@sominos.io>
2022-07-02 17:33:58 -0400

    check in windows serial port lib

--- example/serial_example.dart
+++ example/serial_example.dart
@@ -1,7 +1,21 @@
+import 'package:libserialport/libserialport.dart';
 import 'package:modbus/modbus.dart' as modbus;
 import 'package:modbus/src/modbus_enums.dart';
 
 void serialExample() async {
+  print('Available ports:');
+  var i = 0;
+  for (final name in SerialPort.availablePorts) {
+    final sp = SerialPort(name);
+    print('${++i}) $name');
+    print('\tDescription: ${sp.description}');
+    print('\tManufacturer: ${sp.manufacturer}');
+    print('\tSerial Number: ${sp.serialNumber}');
+    print('\tProduct ID: 0x${sp.productId!.toRadixString(16)}');
+    print('\tVendor ID: 0x${sp.vendorId!.toRadixString(16)}');
+    sp.dispose();
+  }
+
   modbus.ModbusClient? client;
   try {
     client = modbus.createRtuClient(
